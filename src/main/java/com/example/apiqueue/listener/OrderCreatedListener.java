ccc5c173e03680ecefdb2ef883ac5c6beab5aaa7
Arthur Lopes <arthurlopr12@gmail.com>
2024-06-20 20:06:16 -0300

    feat: create listener in RabbitMq queue

--- src/main/java/com/example/apiqueue/listener/OrderCreatedListener.java
+++ src/main/java/com/example/apiqueue/listener/OrderCreatedListener.java
@@ -1,7 +1,12 @@
 package com.example.apiqueue.listener;
 
 import com.example.apiqueue.dto.OrderCreatedEventDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +15,15 @@ import static com.example.apiqueue.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
 @Component
 public class OrderCreatedListener {
 
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEventDTO> message) {
+        logger.info("Message consumed: {}", message);
+    }
+
+    @Bean
+    public Declarable orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE);
     }
 }
